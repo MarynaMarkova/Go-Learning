119f5a1b963f6da4395e93461674f5ecba1c7620
Maryna M <markova.maryna@gmail.com>
2023-02-28 21:15:47 +0200

    add Error checking

--- First_Site/main.go
+++ First_Site/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -8,7 +9,7 @@ import (
 const portNumber = ":8080"
 
 // Home is the home page handler
-func Home(w http.ResponseWriter, r  *http.Request){
+func Home(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "This is the home page")
 }
 //functions with capital letters it's visible outside the package (public analogue), with small letter - only inside the package (private analogue).
@@ -21,7 +22,28 @@ func About(w http.ResponseWriter, r *http.Request){
 	// %d - is a placeholder for int
 }
 
-// addValues adds two integers and return the sum
+// Divide divides one value into another and returns message with result
+func Divide(w http.ResponseWriter, r *http.Request){
+	f, err := divideValues(100.0, 0.0)
+	if err != nil {
+		_, _ = fmt.Fprintf(w, "Cannot divide by 0")
+		return
+	}
+
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", 100.0, 0.0, f))
+}
+
+// divideValues divides one float by another and returs the result of division
+func divideValues(x, y float32) (float32, error) {
+	if y <=0 {
+	err := errors.New("Cannot divide by zero")
+	return 0, err
+	}
+	
+	result := x / y
+	return result, nil
+}
+// addValues adds two integers and returns the sum
 func addValues(x, y int) int {
 	return x + y
 }
@@ -31,6 +53,7 @@ func main() {
 
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
 	fmt.Printf(fmt.Sprintf("Starting application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
