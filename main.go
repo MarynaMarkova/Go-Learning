7a1b609dec7946394b5bdd54a49d29e3d736bc07
Maryna Markova <markova.maryna@gmail.com>
2023-04-26 23:33:42 +0300

    make Eliza run

--- main.go
+++ main.go
@@ -1,14 +1,32 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"myapp/doctor"
+	"os"
+	"strings"
 )
 
 func main() {
-	var whatToSay string
-
-	whatToSay = doctor.Intro()
+	reader := bufio.NewReader(os.Stdin)
+	
+	whatToSay := doctor.Intro()
 
 	fmt.Println(whatToSay)
+
+	for {
+		fmt.Print("-> ")
+		userInput, _ := reader.ReadString('\n')
+// for Windows users
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+// for all the others
+		userInput = strings.Replace(userInput, "\n", "", -1)
+		
+		if userInput == "quit" {
+			break
+			} else {
+			fmt.Println(doctor.Response(userInput))
+		}
+	}
 }
